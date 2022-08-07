895aed1daf70c187bf4394dd96dee6edccbc844c
Indivicivet <indivicivet@gmail.com>
2022-08-07 22:55:13 +0100

    initial index-generating script

---
+++ repo_scripts/update_index.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+index_folder = Path(__file__).resolve().parent.parent
+index_file = index_folder / "index.html"
+
+words_folder_name = "words"
+words = Path(__file__).resolve().parent.parent / words_folder_name
+
+print("generating html")
+html = """
+<html>
+<body>
+"""
+
+for word_file in words.glob("*.*"):
+    html += f"""
+<div class="word">
+    {word_file.stem}
+    <img src="{words_folder_name}/{word_file.name}" />
+</div>
+"""
+
+html += """
+</body>
+</html>
+"""
+
+index_file.parent.mkdir(exist_ok=True, parents=True)
+index_file.write_text(html)
+print(f"successfully wrote {index_file}")
