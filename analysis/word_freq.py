61efa4a870c01f71c6d473f96e9f74b42e7b7ca6
indi <indivicivet@gmail.com>
2023-10-08 13:52:23 +0100

    add word cloud and plot

--- analysis/word_freq.py
+++ analysis/word_freq.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import re
 from collections import Counter
 
+import matplotlib.pyplot as plt
+import wordcloud
+
 
 def do_analysis(filename):
     all_text = Path(filename).read_text()
@@ -9,6 +12,15 @@ def do_analysis(filename):
     words = [x.strip() for x in just_letters.split()]
     counts = Counter(words)
     print(counts)
+    print(f"{len(counts)} distinct words")
+    wordcloud.WordCloud(
+        width=1280,
+        height=720,
+        max_words=1000,
+        prefer_horizontal=1,
+    ).generate_from_frequencies(counts).to_image().show()
+    plt.bar(*zip(*counts.most_common()))
+    plt.show()
 
 
 if __name__ == "__main__":
