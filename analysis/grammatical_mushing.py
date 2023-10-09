c772ee984414ad060a33653e46e001c5fd3a2534
indi <indivicivet@gmail.com>
2023-10-09 22:43:16 +0100

    various different mushing

--- analysis/grammatical_mushing.py
+++ analysis/grammatical_mushing.py
@@ -3,9 +3,16 @@ from pathlib import Path
 
 def do_stuff(file):
     txt = Path(file).read_text()
-    txt = txt.replace(" e ", ">>")
-    txt = txt.replace(" pi ", ";")
-    return txt
+    sents = [
+        s.strip()
+        for line in txt.splitlines()
+        for s in line.split(".")
+        if s.strip()
+    ]
+    for i, s in enumerate(sents):
+        s2 = s.replace(" e ", ">>").replace(" pi ", ";")
+        sents[i] = s2
+    return ". ".join(sents)
 
 
 if __name__ == "__main__":
